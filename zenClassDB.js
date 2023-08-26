3696d5086d09ccd851da85c1a6b7f1563b64006e
prathu99 <79712844+prathu99@users.noreply.github.com>
2023-08-26 16:06:24 +0530

    Add files via upload

---
+++ zenClassDB.js
@@ -0,0 +1,22 @@
+[{
+    "name": "Zenclass Program",
+    "program": "MERN Stack",
+    "mentors": [{
+        "name": "ABC 1",
+        "module": "HTML,CSS & Javascript",
+        "mentee_count": 100
+    },
+    {
+        "name": "ABC 2",
+        "module": "Data Structures & Algorithms",
+        "mentee_count": 70,
+    },
+    {
+        "name": "ABC 3",
+        "module": "ReactJS & Databases",
+        "mentee_count": 30,
+    }],
+    "users": {
+        "name": "prathamesh",
+    }
+}];
